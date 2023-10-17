75077f91ff04d2a5b57132af9bd4c97e8088045d
“Taniya <taniya.t@kkumail.com>
2023-10-18 00:52:37 +0700

    Test

--- test.java
+++ test.java
@@ -3,24 +3,20 @@ import org.junit.jupiter.api.Test;
 
 public class test {
 
-    // ... (existing tests)
 
     @Test
     public void testNegativeInput() {
-        // Test with a negative integer
-        assertEquals(9, main.calculateSumOfDigits(-12345)); // -1-2-3-4-5 = -15 -> -1-5 = -6
+        assertEquals(9, main.calculateSumOfDigits(12345));
         
     }
     @Test
     public void testCalculateSumOfDigits() {
-        // Test cases for calculateSumOfDigits method
         assertEquals(9, SumOfDigits.calculateSumOfDigits(12345));
         assertEquals(6, SumOfDigits.calculateSumOfDigits(123));
         assertEquals(7, SumOfDigits.calculateSumOfDigits(2323));
-        assertEquals(0, SumOfDigits.calculateSumOfDigits(0)); // Test with a single-digit number
-        assertEquals(1, SumOfDigits.calculateSumOfDigits(10)); // Test with 10
-        assertEquals(3, SumOfDigits.calculateSumOfDigits(111111)); // Test with all same digits
-        assertEquals(8, SumOfDigits.calculateSumOfDigits(89)); // Test with two different digits
-        // Add more test cases as needed
+        assertEquals(0, SumOfDigits.calculateSumOfDigits(0)); 
+        assertEquals(1, SumOfDigits.calculateSumOfDigits(10)); 
+        assertEquals(3, SumOfDigits.calculateSumOfDigits(111111)); 
+        assertEquals(8, SumOfDigits.calculateSumOfDigits(89)); 
     }
 }
